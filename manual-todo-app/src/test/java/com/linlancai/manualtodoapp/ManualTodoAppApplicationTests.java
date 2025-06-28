edb68486b6d484f6817be2b1ed00bcd2534a9fab
LINLAN CAI <liana255c@outlook.com>
2025-06-27 20:58:47 -0400

    todo-app redo

---
+++ manual-todo-app/src/test/java/com/linlancai/manualtodoapp/ManualTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linlancai.manualtodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManualTodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
